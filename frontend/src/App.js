a0fb5b01fcb8db7dc9fbe1b7f186941f51a98adf
Jungal Mungal <Jungla@gmail.com>
2023-02-03 22:26:40 +0500

    version 1

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React 2222333344444 77777 8888 0000
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
